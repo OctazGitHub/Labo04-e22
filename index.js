e4aaef8d6c3ab7ba649b877c81abc854799b2974
Raphael Dore <raphbuisness@outlook.com>
2022-08-17 14:30:15 -0400

    ajout de la library personne.js et codge de index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Salut tout le monde")
+console.log("Salut tout le monde");
+
+const personne = require('./personne');
+console.log(personne.nom);
+console.log('tu as '+personne.age)+ ' ans';
+
