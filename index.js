9897024e26ffaea7864cf7ee8d4fd07b9766c58a
Raphael Dore <raphbuisness@outlook.com>
2022-08-17 14:44:33 -0400

    sheeesshh

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const personne = require('./personne');
 console.log(personne.nom);
 console.log('tu as '+personne.age)+ ' ans';
 
-const Personne = require('./classPersonne')
-const paul = new Personne('Paul Larue',38)
+const Personne = require('./classPersonne');
+const paul = new Personne('Paul Larue',38);
 paul.salut();
+var bob = new Personne('Bob Larue',33);
+bob.salut();
